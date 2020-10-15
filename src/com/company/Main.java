700bf151ea226ebed361aca6e3c79bdf7365d6ff
Jamal <jamalcwoods@gmail.com>
2020-10-15 18:00:59 -0400

    java.sql imported

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,17 @@
 package com.company;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 	// write your code here
+
+        /*
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String test = reader.readLine();
 
@@ -15,5 +19,8 @@ public class Main {
             System.out.println(test);
             test = reader.readLine();
         } while(test != "exit");
+        */
+
+        Connection con = DriverManager.getConnection();
     }
 }
